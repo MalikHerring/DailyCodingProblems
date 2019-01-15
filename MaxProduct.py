66a2a2af50027ec2161f9bf6bd2db46bdce2b194
Malik Herring <malikherring@u.boisestate.edu>
2019-01-15 15:32:40 -0700

    Create MaxProduct.py -- Problem #69
    This problem was from DailyCodingProblems, it successfully implements the task needed which was find the highest product of any 3 numbers. To solve this problem I decided to sort the list, and multiply the lowest 2 lowest negative numbers and the highest positive number; and compare it to multiplying the 3 highest numbers, and returning the greater of the two.

---
+++ MaxProduct.py
@@ -0,0 +1,18 @@
+def DailyCodingProblem(int_list):
+  """Finds the largest product of any 3 numbers from a list
+  
+  Given a list of integers, return the largest product that 
+  can be made by multiplying any three integers.
+  
+  Args:
+    int_list: A list of integers.
+    
+  Returns:
+    None if the list is too small, else the highest product
+  """  
+	if len(int_list) < 3:
+		return None
+	int_list = sorted(int_list)
+	lower = int_list[0] * int_list[1] * int_list[-1]
+	upper = int_list[-1] * int_list[-2] * int_list[-3]
+	return (lower if lower > upper else upper)
