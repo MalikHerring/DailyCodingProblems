418d672e5fd8a7b0fc4124f5fc4485050735e5f6
Malik Herring <malikherring@u.boisestate.edu>
2019-01-15 14:29:28 -0700

    Updated Product List to follow Google Documenting

--- ProductList.py
+++ ProductList.py
@@ -1,9 +1,27 @@
-"""Problem #2: Given an array of integers, return a new array such that each element at index i 
-of the new array is the product of all the numbers in the original array except the one at i."""
+"""Problem #2 from DailyCodingProblem.com
+
+Given an array of integers, return a new array such that each element at index i 
+of the new array is the product of all the numbers in the original array except the one at i.
+
+"""
 
 def DailyCodingProblem(lt):
-	"""Given an array of integers, return a new array such that each element at index i of the 
-  new array is the product of all the numbers in the original array except the one at i."""
+	""" Generates a list of products.
+		
+	Given an array of integers, return a new array such 
+	that each element at index i of the new array is the 
+	product of all the numbers in the original array except 
+	the one at i.
+	
+	Args: 
+		lt: List of integers.
+		
+	Returns:
+		Returns a new list of integers where each index
+		is the product of all numbers besides the number
+		at that index from lt.
+
+	"""
 	product = 1
 	ret_list = []
 	for x in lt:
