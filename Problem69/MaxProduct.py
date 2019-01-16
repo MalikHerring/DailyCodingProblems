0556d2a28839f138edfacdc924b728b908a62dd5
Malik Herring <malikherring@u.boisestate.edu>
2019-01-16 10:42:57 -0700

    Update MaxProduct.py

--- Problem69/MaxProduct.py
+++ Problem69/MaxProduct.py
@@ -1,3 +1,11 @@
+"""Problem #69 from DailyCodingProblem.com
+
+Given a list of integers, return the largest product that can be made 
+by multiplying any three integers. For example, if the list is 
+[-10, -10, 5, 2], we should return 500, since that's -10 * -10 * 5.
+
+"""
+
 def max_product(int_list):
   """Finds the largest product of any 3 numbers from a list
   
