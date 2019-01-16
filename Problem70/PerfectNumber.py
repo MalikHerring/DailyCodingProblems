b5a802d946351a57f1cac5ee6ea8e15b86a75332
Malik Herring <malikherring@u.boisestate.edu>
2019-01-16 10:43:55 -0700

    Update PerfectNumber.py

--- Problem70/PerfectNumber.py
+++ Problem70/PerfectNumber.py
@@ -1,3 +1,11 @@
+"""Problem #70 from DailyCodingProblems.com
+
+A number is considered perfect if its digits sum up to exactly 10.
+Given a positive integer n, return the n-th perfect number.
+For example, given 1, you should return 19. Given 2, you should return 28.
+
+"""
+
 def perfect_number(base):
   """Creates a perfect number given a base positive integer
   
