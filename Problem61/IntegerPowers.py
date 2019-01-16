53d288158c21bee1b241069a004e95ea425ca081
Malik Herring <malikherring@u.boisestate.edu>
2019-01-16 10:31:54 -0700

    Update and rename IntegerPowers.py to Problem61/IntegerPowers.py

---
+++ Problem61/IntegerPowers.py
@@ -0,0 +1,23 @@
+def intpow(base, power):
+	"""Used to calculate integer exponentiation
+	
+	Given a base and a power we use a recursive method that
+	maintains a speed faster than a for loop of repeated 
+	multiplication as we increase it by double every time.
+	
+	Args:
+		base: The base number we are exponentiating
+		power: Number of times we multiply base by itself, must be positive
+	
+	Returns:
+		This returns base^power
+	
+	"""
+	if power is 0:
+		return 1
+	if power is 1:
+		return base
+	if (power & 1) is not 0:
+		return base * intpow(base * base, power // 2)
+	else:
+		return intpow(base * base, power // 2)
